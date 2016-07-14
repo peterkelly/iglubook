d02017e34d61fd4cfec6e81872fbcebac1c07583
Peter Kelly <peter@uxproductivity.com>
2016-07-14 23:47:07 +0700

    Work

---
+++ www/js/api.js
@@ -0,0 +1,201 @@
+var fakeTimeout = 1000;
+
+var sampleUsers = [
+    {
+        id: 1,
+        email: "jennifer@iglubook.com",
+        password: "",
+        firstName: "Jennifer",
+        lastName: "Lawrence",
+        gender: "F",
+        country: "US",
+    },
+    {
+        id: 2,
+        email: "robert@iglubook.com",
+        password: "",
+        firstName: "Robert Downey Jr",
+        lastName: "Downey Jr",
+        gender: "M",
+        country: "US",
+    },
+    {
+        id: 3,
+        email: "leo@iglubook.com",
+        password: "",
+        firstName: "Leonardo",
+        lastName: "DiCaprio",
+        gender: "M",
+        country: "US",
+    },
+    {
+        id: 4,
+        email: "brad@iglubook.com",
+        password: "",
+        firstName: "Bradley",
+        lastName: "Cooper",
+        gender: "M",
+        country: "US",
+    },
+    {
+        id: 5,
+        email: "the@rock.com",
+        password: "",
+        firstName: "Dwayne",
+        lastName: "Johnson",
+        gender: "M",
+        country: "US",
+    },
+    {
+        id: 6,
+        email: "hugh@iglubook.com",
+        password: "",
+        firstName: "Hugh",
+        lastName: "Jackman",
+        gender: "M",
+        country: "AU",
+    },
+    {
+        id: 7,
+        email: "sandra@iglubook.com",
+        password: "",
+        firstName: "Sandra",
+        lastName: "Bullock",
+        gender: "F",
+        country: "US",
+    },
+    {
+        id: 8,
+        email: "scarlett@iglubook.com",
+        password: "",
+        firstName: "Scarlett",
+        lastName: "Johansson",
+        gender: "F",
+        country: "US",
+    },
+];
+
+var samplePosts = [
+    {
+        id: 1,
+        posterFullName: "Jennifer Lawrence",
+        date: "10 July 10:14am",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam volutpat "+
+        "egestas pretium. Nunc pellentesque venenatis felis sit amet ullamcorper. Phasellus arcu "+
+        "lacus, placerat eu est quis, bibendum hendrerit dolor. Sed ac sem tellus. Mauris "+
+        "tristique, ante ut sodales placerat, nisi ex faucibus elit, eu pretium felis mi et metus.",
+        likes: 4,
+        comments: 0,
+    },
+    {
+        id: 2,
+        posterFullName: "Robert Downey Jr",
+        date: "11 July 2:35pm",
+        content: "Maecenas nec mi auctor, euismod ligula imperdiet, vulputate nisi. Vivamus "+
+        "luctus eros et mattis faucibus. Suspendisse potenti. In hac habitasse platea dictumst. "+
+        "Ut venenatis, lacus vel suscipit placerat, leo ipsum tincidunt dui, quis pellentesque "+
+        "nibh justo et est.",
+        likes: 17,
+        comments: 5,
+    },
+    {
+        id: 3,
+        posterFullName: "Leonardo DiCaprio",
+        date: "11 July 4:20pm",
+        content: "Fusce felis nisl, cursus quis malesuada sed, varius et sapien. Pellentesque "+
+        "sed porta turpis. Morbi varius enim et massa tristique, et fermentum purus congue.",
+        likes: 11,
+        comments: 3,
+    },
+    {
+        id: 4,
+        posterFullName: "Bradley Cooper",
+        date: "11 July 5:50pm",
+        content: "Etiam luctus arcu at tincidunt pellentesque. Donec eu quam risus. Cras "+
+        "commodo sapien at diam gravida lobortis. Quisque ac nisl eu nisi fringilla blandit eu "+
+        "ac elit. Mauris commodo eleifend mattis. Praesent volutpat fringilla dictum. Etiam "+
+        "viverra ultricies dolor ut bibendum. Sed ullamcorper pretium sem nec suscipit.",
+        likes: 0,
+        comments: 19,
+    },
+    {
+        id: 5,
+        posterFullName: "Dwayne Johnson",
+        date: "12 July 11:20am",
+        content: "Aliquam rutrum dignissim nibh. Donec in metus nec lacus sodales imperdiet. "+
+        "Mauris cursus blandit magna, a accumsan felis porttitor et. Etiam a libero a ante "+
+        "tincidunt fringilla id et ante. Mauris a lacus eu eros pharetra imperdiet id quis metus. "+
+        "Fusce at libero dui.",
+        likes: 6,
+        comments: 2,
+    },
+    {
+        id: 6,
+        posterFullName: "Hugh Jackman",
+        date: "12 July 12:39pm",
+        content: "Nam suscipit consequat nulla, vitae sodales magna mattis blandit. Donec "+
+        "vehicula mattis quam, nec sodales tellus sagittis interdum.",
+        likes: 10,
+        comments: 6,
+    },
+    {
+        id: 7,
+        posterFullName: "Sandra Bullock",
+        date: "13 July 15:22pm",
+        content: "Donec id augue dapibus, imperdiet purus id, condimentum augue. Cras sodales "+
+        "enim magna, ut fringilla justo luctus sit amet. Ut ornare gravida porttitor. Vivamus sit "+
+        "amet augue aliquet, finibus augue nec, pharetra metus. Vestibulum non nisi id lorem "+
+        "maximus feugiat eget ut ex.",
+        likes: 3,
+        comments: 3,
+    },
+    {
+        id: 8,
+        posterFullName: "Scarlett Johansson",
+        date: "13 July 18:59pm",
+        content: "Maecenas fringilla ornare consectetur. Vestibulum at laoreet augue. Duis id "+
+        "fringilla massa, vitae imperdiet leo. Pellentesque eu gravida diam. Morbi venenatis "+
+        "pellentesque nisl ac porta. Donec sollicitudin urna massa, ac molestie urna cursus in.",
+        likes: 15,
+        comments: 6,
+    },
+];
+
+
+app.service("api",function($timeout,$q,$http) {
+    return {
+        getFeedContents: function() {
+            return $q(function(resolve,reject) {
+                $timeout(function() {
+                    resolve(samplePosts);
+                },fakeTimeout);
+            });
+        },
+        getFriends: function() {
+            return $q(function(resolve,reject) {
+                $timeout(function() {
+                    resolve(sampleUsers);
+                },fakeTimeout);
+            });
+        },
+        getUser: function() {
+            return $q(function(resolve,reject) {
+                $timeout(function() {
+                    resolve(sampleUsers[0]);
+                },fakeTimeout);
+            });
+        },        // getFeedContents: function() {
+        //     return $q(function(resolve,reject) {
+        //         $http({
+        //             method: "GET",
+        //             url: "http://localhost:3000/posts",
+        //         }).then(function(response) {
+        //             console.log("Resposne",response);
+        //             resolve(response.data);
+        //         }).catch(function(error) {
+        //             reject(error);
+        //         });
+        //     });
+        // }
+    }
+});
--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic']);
 
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -21,20 +21,31 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
 
-.config(function ($stateProvider,$urlRouterProvider) {
+app.config(function ($stateProvider,$urlRouterProvider) {
 
+    // Controlers are specified in the templates, since two-way data binding does not get set up
+    // correctly if we specify them here for some reason
     $stateProvider
-    .state("login",{ url: "/login", templateUrl: "login.html" })
-    .state("signup",{ url: "/signup", templateUrl: "signup.html" })
-    .state("main",{ url: "/main", templateUrl: "main.html", controller: "MainCtrl", abstract: true })
+    .state("login",{
+        url: "/login",
+        templateUrl: "login.html",
+    })
+    .state("signup",{
+        url: "/signup",
+        templateUrl: "signup.html",
+    })
+    .state("main",{
+        url: "/main",
+        templateUrl: "main.html",
+        abstract: true
+    })
     .state("main.feed",{
         url: "/feed",
         views: {
             "content": {
                 templateUrl: "feed.html",
-                controller: "FeedCtrl",
             }
         }
     })
@@ -43,7 +54,6 @@ angular.module('starter', ['ionic'])
         views: {
             "content": {
                 templateUrl: "comments.html",
-                controller: "CommentsCtrl",
             }
         }
     })
@@ -52,7 +62,6 @@ angular.module('starter', ['ionic'])
         views: {
             "content": {
                 templateUrl: "profile.html",
-                controller: "ProfileCtrl",
             }
         }
     })
@@ -61,7 +70,6 @@ angular.module('starter', ['ionic'])
         views: {
             "content": {
                 templateUrl: "friends.html",
-                controller: "FriendsCtrl",
             }
         }
     })
@@ -70,47 +78,51 @@ angular.module('starter', ['ionic'])
         views: {
             "content": {
                 templateUrl: "logout.html",
-                controller: "LogoutCtrl",
             }
         }
     })
 
     $urlRouterProvider.otherwise("/login")
-})
+});
 
-.service("api",function($timeout,$q,$http) {
-    return {
-        // getFeedContents: function() {
-        //     return $q(function(resolve,reject) {
-        //         $timeout(function() {
-        //             resolve([
-        //                 { id: 1, content: "First post" },
-        //                 { id: 2, content: "Second post" },
-        //                 { id: 3, content: "Third post" },
-        //                 { id: 4, content: "Fourth post" },
-        //                 { id: 5, content: "Fifth post" },
-        //                 { id: 6, content: "Sixth post" },
-        //             ]);
-        //         },2000);
-        //     });
-        // }
-        getFeedContents: function() {
-            return $q(function(resolve,reject) {
-                $http({
-                    method: "GET",
-                    url: "http://localhost:3000/posts",
-                }).then(function(response) {
-                    console.log("Resposne",response);
-                    resolve(response.data);
-                }).catch(function(error) {
-                    reject(error);
-                });
-            });
-        }
-    }
-})
+app.controller("LoginCtrl",function($scope,$state,$timeout,$ionicLoading) {
 
-.controller("FeedCtrl",function($scope,$ionicLoading,$timeout,api) {
+    $scope.email = "";
+    $scope.password = "";
+
+    $scope.$watch("email",function() {
+        console.log("Email changed: "+JSON.stringify($scope.email));
+    });
+
+    $scope.$watch("password",function() {
+        console.log("Password changed: "+JSON.stringify($scope.password));
+    });
+
+    $scope.loginPressed = function() {
+        console.log("Log in pressed");
+        console.log("email = "+$scope.email);
+        console.log("password = "+$scope.password);
+        // $state.go("main.feed");
+        $ionicLoading.show().then(function() {
+            $timeout(function() {
+                $ionicLoading.hide();
+                $state.go("main.feed");
+            },1500);
+        });
+    };
+
+    $scope.signupPressed = function() {
+        console.log("Sign up pressed");
+        $state.go("signup");
+    };
+
+});
+
+app.controller("SignupCtrl",function($scope) {
+
+});
+
+app.controller("FeedCtrl",function($scope,$ionicLoading,$timeout,api) {
     // $scope.viewTitle = "Feed";
     // console.log("feed: viewTitle = "+$scope.viewTitle);
     // $scope.viewTitle = "Known";
@@ -132,7 +144,7 @@ angular.module('starter', ['ionic'])
 
             },2000);
         });
-    }
+    };
 
     $scope.doRefresh = function() {
         api.getFeedContents().then(function(posts) {
@@ -143,32 +155,67 @@ angular.module('starter', ['ionic'])
         }).finally(function() {
             $scope.$broadcast("scroll.refreshComplete");
         });
-    }
-})
+    };
 
-.controller("CommentsCtrl",function($scope) {
+    $scope.doRefresh();
+});
+
+app.controller("CommentsCtrl",function($scope) {
     // $scope.viewTitle = "Comments";
     $scope.viewTitle.value = "Comments";
-})
+});
 
-.controller("ProfileCtrl",function($scope) {
+app.controller("ProfileCtrl",function($scope,$ionicLoading,$timeout,api) {
     $scope.viewTitle.value = "Profile";
-})
 
-.controller("FriendsCtrl",function($scope) {
+    $scope.user = null;
+    $scope.notifications = true;
+    $scope.test = null;
+
+    $scope.updatePressed = function() {
+        $ionicLoading.show({ template: "Saving changes..." }).then(function() {
+            $timeout(function() {
+                $ionicLoading.hide();
+            },1000);
+        })
+    }
+
+    api.getUser().then(function(user) {
+        $scope.user = user;
+    });
+
+});
+
+app.controller("FriendsCtrl",function($scope,api) {
     $scope.viewTitle.value = "Friends";
-})
+    $scope.friends = null;
 
-.controller("LogoutCtrl",function($scope) {
+    api.getFriends().then(function(friends) {
+        $scope.friends = friends;
+    });
+});
+
+app.controller("LogoutCtrl",function($scope) {
     $scope.viewTitle.value = "Logout";
-})
+});
 
-.controller("MainCtrl",function($scope,$ionicSideMenuDelegate,$state,$timeout) {
+app.controller("MainCtrl",function($scope,$ionicSideMenuDelegate,$state,$timeout) {
+
     $scope.viewTitle = { value: "Unknown!!!!" };
+
     $scope.toggleMenu = function() {
         $ionicSideMenuDelegate.toggleLeft();
     }
-})
+
+    $scope.logoutPressed = function() {
+        $state.go("login");
+    }
+
+    $scope.composePressed = function() {
+        console.log("Compose pressed");
+    }
+
+});
 
 
 /*
